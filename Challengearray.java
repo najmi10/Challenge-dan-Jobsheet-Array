7fc2c5b32b5faaff9c94c43dfcdf073eacefdc9f
najmi10 <53500211+najmi10@users.noreply.github.com>
2019-09-12 11:22:22 +0700

    Add files via upload

---
+++ Challengearray.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package challenge1;
+
+/**
+ *
+ * @author ica
+ */
+public class Challengearray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[]args){
+       String [] nama = {"Sabrina Amelia Royantika Putri", "Nesya Ari Wijayanti",
+           "Natasya Adelia Ramadhanti", "Larasati Puspita Candra Dewi", "Rensi Meila Yulvinata",
+           "Rendi Prayata Wibawa", "Memoreza Sabana", "James Hestu Satria",
+           "Ricmond Janus Rafi'i Aryanto", "Ruqul Adam Himawan"};
+       String [] alamat = {"Tulungagung", "Malang", "Blitar", "Kediri","Kediri",
+           "Tuban", "Tuban", "Malang", "Sidoarjo", "Blitar"};
+       String [] nohp = {"082331381865", "085335712099", "085646302778", "081235641977",
+           "082334929755", "085804146990", "085736041408", "082165319310", "085236614826",
+           "0859193774917"};
+       String [] hobi = {"Tidur", "Menunggu tanpa kepastian", "Membucin", "Menyanyi",
+           "Stalking", "Makan", "Travelling", "Bermain Basket", "Jogging", "Membucin"};
+       String [] ig = {"@sabrinampan", "@nesyarw", "@ntsyade", "@laraspcd_", "@rensimyl",
+           "@rendiwibawa17", "@ozzas_", "@mr.sleepless_owl", "@raviiariyanto", "@adam.hmwan"};
+       
+       System.out.print("===Identitas siswa kelas X RPL 2===");
+       System.out.println(" ");
+       
+       System.out.println("nama : " + nama[1]);
+       System.out.println("alamat : " + alamat[1]);
+       System.out.println("nohp : " + nohp[1]);
+       System.out.println("hobi : " + hobi[1]);
+       System.out.println("ig : " + ig[1]);
+       System.out.println(" ");
+       
+       System.out.println("nama : " + nama[7]);
+       System.out.println("alamat : " + alamat[7]);
+       System.out.println("nohp : " + nohp[7]);
+       System.out.println("hobi : " + hobi[7]);
+       System.out.println("ig : " + ig[7]);
+       System.out.println(" ");
+               }
+       
+           
+       
+               
+               
+       }
+       
+       
+       
+       
+               
+    
+    
+
---
+++ Temansederet.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temansederet;
+
+/**
+ *
+ * @author ica
+ */
+public class Temansederet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String[] nama = new String[7];
+       
+       //membuat array
+       nama[0] = "Yulius";
+       nama[1] = "Nurisa";
+       nama[2] = "Sabrina";
+       nama[3] = "Moza";
+       nama[4] = "Rendi";
+       nama[5] = "Fara";
+       nama[6] = "Rensi";
+       
+       //mengambil data array
+       System.out.print("nama teman pertama saya: " + nama[4] +"nama teman kedua saya: " + nama[0] +"nama teman ketiga saya: " + nama[2] + "\n");
+       
+       
+    }
+    
+}
